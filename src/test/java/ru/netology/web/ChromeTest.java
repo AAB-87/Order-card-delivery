5d3bffbb3098cc67cc705150548e2aa63c14b685
AAB-87 <andrej-abrekov@yandex.ru>
2021-12-07 22:52:12 +0400

    added sendKeys

--- src/test/java/ru/netology/web/ChromeTest.java
+++ src/test/java/ru/netology/web/ChromeTest.java
@@ -1,6 +1,7 @@
 package ru.netology.web;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import java.time.Duration;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -10,8 +11,10 @@ public class ChromeTest {
 
     @Test
     public void shouldBeAppointmentMeeting() {
+
         open("http://localhost:9999"); // открываем страницу
         $("[data-test-id = 'city'] input").setValue("Краснодар"); // находим элемент (поле "Город") и вводим туда текст
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.COMMAND, "a", Keys.DELETE)); // предварительная чистка поля с датой по умолчанию
         $("[data-test-id = 'date'] input").setValue("15.12.2021"); // находим элемент (поле "Дата") и вводим туда текст
         $("[data-test-id = 'name'] input").setValue("Шишкин Алексей"); // находим элемент (поле "Фамилия Имя") и вводим туда текст
         $("[data-test-id = 'phone'] input").setValue("+79371458228"); // находим элемент (поле "Телефон") и вводим туда текст
