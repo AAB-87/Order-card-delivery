36509350f85209bfd69dde04e16e57e59b967ea8
AAB-87 <andrej-abrekov@yandex.ru>
2021-12-07 20:39:56 +0400

    Deleted skeleton

--- src/test/java/ru/netology/web/ChromeTest.java
+++ src/test/java/ru/netology/web/ChromeTest.java
@@ -16,28 +16,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ChromeTest {
-    private WebDriver driver;// основная сущность c полем драйвер
-
-    @BeforeAll
-    public static void setUpClass() { // метод вызывается 1 раз перед проходом всех тестов
-        WebDriverManager.chromedriver().setup(); // указываем что используем и где оно у нас лежит (путь)
-    }
-
-    @BeforeEach // выполняется перед каждым тестом
-    public void setupTest() { // перед каждым тестом создаём хром драйвер
-        ChromeOptions options = new ChromeOptions(); // в режиме headless мы отключаем графический интерфейс браузера (при этот все процессы браузера продолжают работать так же)
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach // выполняется после каждого теста
-    public void tearDown() { // после каждого теста закрываем браузер и присваиваем null полю driver
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 
     @Test
     public void shouldBeAppointmentMeeting() {
